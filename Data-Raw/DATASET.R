c3808e3cf2657ace3727a3f288333291448e5f31
Quinn Chamblin <qchamblin@smith.edu>
2024-04-17 09:22:33 -0400

    got rid of NA rows and columns

---
+++ Data-Raw/DATASET.R
@@ -0,0 +1,10 @@
+## code to prepare `DATASET` dataset goes here
+
+usethis::use_data(DATASET, overwrite = TRUE)
+
+library(readr)
+US_Open_Finals_ <- read_csv("Data-Raw/US Open Finals .csv")
+View(US_Open_Finals_)
+
+# Cleaning
+tennis <- US_Open_Finals_[c(1:14), c(1:8)]
