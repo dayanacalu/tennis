3a77f479f187dd26ab093fedd0317fcce62b74ae
Quinn Chamblin <qchamblin@smith.edu>
2024-04-17 20:35:27 -0400

    fixed code for overwrite

--- Data-Raw/DATASET.R
+++ Data-Raw/DATASET.R
@@ -1,10 +1,9 @@
-## code to prepare `DATASET` dataset goes here
-
-usethis::use_data(DATASET, overwrite = TRUE)
-
 library(readr)
 US_Open_Finals_ <- read_csv("Data-Raw/US Open Finals .csv")
 View(US_Open_Finals_)
 
 # Cleaning
 tennis <- US_Open_Finals_[c(1:14), c(1:8)]
+## code to prepare `DATASET` dataset goes here
+usethis::use_data(tennis, overwrite = TRUE)
+
