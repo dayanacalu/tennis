d916536c20982f7d436a1795dc3b1dd0ecc718bf
ydavidson <y.may.davidson@gmail.com>
2024-04-22 09:07:41 -0400

    committing to pull

--- R/Tennis.R
+++ R/Tennis.R
@@ -9,8 +9,4 @@ install.packages("broom")
 library(broom)
 predictions <- augment(tennis_lm, interval = "prediction")
 
-# Function
-tennis_predict <- function(player1_aces, player2_aces) {
-  difference_aces <- player1_aces - player2_aces
-  if ()
-}
+
