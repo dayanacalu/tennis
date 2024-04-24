6bbcefb8a686e245d816bfb3898a237c30389e38
Dayanacalu <dcaluna@smith.edu>
2024-04-24 09:17:38 -0400

    Roxy

--- R/Tennis.R
+++ R/Tennis.R
@@ -1,5 +1,18 @@
+#Predict the winner of a tennis match based on the number of aces each player serves.
 
-# Function DRAFT
+#This function compares the number of aces served by two players and predicts the winner
+#based on the difference in aces. If the difference is positive, it predicts that Player 1 wins.
+#If the difference is negative, it predicts that Player 2 wins. If the difference is zero, it
+#cannot predict the winner.
+#
+#' @param player1_aces Number of aces served by Player 1.
+#' @param player2_aces Number of aces served by Player 2.
+#' @return A character string indicating the predicted winner: "Player 1 wins", "Player 2 wins", or "We can't predict who wins".
+#' @examples
+#' tennis_predict(10, 8)
+#' tennis_predict(5, 7)
+#
+#' @export
 tennis_predict <- function(player1_aces, player2_aces){
   difference_aces <- player1_aces - player2_aces
   if (difference_aces > 0){
@@ -12,11 +25,11 @@ tennis_predict <- function(player1_aces, player2_aces){
 }
 
 # Fitting model:
-tennis_lm <- lm(`Difference in Ace` ~ `Player 1 Wins`, data = tennis)
-summary(tennis_lm)
+#tennis_lm <- lm(`Difference in Ace` ~ `Player 1 Wins`, data = tennis)
+#summary(tennis_lm)
 
 # Prediction:
-install.packages("broom")
-library(broom)
-predictions <- augment(tennis_lm, interval = "prediction")
+#install.packages("broom")
+#library(broom)
+#predictions <- augment(tennis_lm, interval = "prediction")
 
