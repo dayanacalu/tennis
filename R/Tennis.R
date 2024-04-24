bd28efae50afd65bc7e09db69a8e6756699c024b
Dayanacalu <dcaluna@smith.edu>
2024-04-24 12:47:20 -0400

    Package Function Done

--- R/Tennis.R
+++ R/Tennis.R
@@ -39,12 +39,3 @@ tennis_predict <- function(player1_aces, player2_aces){
     return ("We can't predict who wins")
   }
 }
-
-# Fitting model:
-#tennis_lm <- lm(`Difference in Ace` ~ `Player 1 Wins`, data = tennis)
-#summary(tennis_lm)
-
-# Prediction:
-#install.packages("broom")
-#library(broom)
-#predictions <- augment(tennis_lm, interval = "prediction")
