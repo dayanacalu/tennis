17ba9a042596e9ab5e7fc6fead2ec13115b8e828
Dayanacalu <dcaluna@smith.edu>
2024-04-25 09:22:08 -0400

    ReadME good

--- R/Tennis.R
+++ R/Tennis.R
@@ -12,23 +12,7 @@
 #' tennis_predict(5, 7)
 #
 #' @export
-
-# Fitting model:
-tennis_lm <- lm(`Difference in Ace` ~ `Player 1 Wins`, data = tennis)
-summary(tennis_lm)
-
-
-#Prediction:
-install.packages("broom")
-library(broom)
-predictions <- augment(tennis_lm, interval = "prediction")
-
-
-# Function DRAFT
-
-#' @title Predcit match outcomes using aces
-#' @description Given the aces of two player the function finds the differences in aces and uses the difference to compute the
-
+#'
 tennis_predict <- function(player1_aces, player2_aces){
   difference_aces <- player1_aces - player2_aces
   if (difference_aces > 0){
@@ -39,3 +23,8 @@ tennis_predict <- function(player1_aces, player2_aces){
     return ("We can't predict who wins")
   }
 }
+
+#Prediction:
+# install.packages("broom")
+# library(broom)
+# predictions <- augment(tennis_lm, interval = "prediction")
