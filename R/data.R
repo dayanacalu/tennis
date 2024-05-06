8083448af9636c2cb60c9919cae58f05140b0ae0
ydavidson <y.may.davidson@gmail.com>
2024-05-06 14:37:14 -0400

    removed characters from line to make lintr happy

--- R/data.R
+++ R/data.R
@@ -7,7 +7,7 @@
 #' \item{`Player 1 Wins`}{Says ‘yes’ if player 1 won the match and ‘no’ if player 2 won the match}
 #' \item{`Player 1 Ace`}{How many aces player 1 served in the match}
 #' \item{`Player 2 Ace`}{How many aces player 2 served in the match}
-#' \item{`Difference in Ace`}{What is the difference in the number of aces between player 1 and player 2}
+#' \item{`Difference in Ace`}{What is the difference in the number of aces between each player}
 #' \item{Year}{The year the match took place}
 #' }
 #' @source <https://www.ultimatetennisstatistics.com/tournament?tournamentId=19&tab=events>
